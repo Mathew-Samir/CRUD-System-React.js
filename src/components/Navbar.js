6192cd8966c31e852dfc925f9efe3d77681414de
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-06-15 17:50:49 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import logo from '../Images/logo.png';
 
 const Navbars = () => {
     return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
           <Navbar.Brand >
-            <Link to={"/"}>Logo </Link>
+            <Link to={"/"}><img src={logo} alt="Logo" /></Link>
           </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
