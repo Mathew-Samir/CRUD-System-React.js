fb5a3f82f79d4dee71be0bd65941cb5358f4e78e
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-06-15 16:01:00 +0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import './Home.css'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+  <>
+    <Carousel fade>
+      <Carousel.Item>
+          <img src="https://www.internetcommercesummit.com/storage/2023/09/ecommerce1.jpg" alt="placeholder" text="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+          <img src="https://pix4free.org/assets/library/2021-06-16/originals/commercial.jpg" alt="placeholder" text="Second slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+          <img src="https://www.vocso.com/blog/wp-content/uploads/2022/02/eCommerce-Website-Features-1920-x-1080.jpg" alt="placeholder" text="Third slide" />
+      </Carousel.Item>
+    </Carousel>
+  </>
   )
 }
 
