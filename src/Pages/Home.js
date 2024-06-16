6f54d93a8c048e6152786ccb1667efc1d337fb9f
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-06-16 11:36:25 +0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,13 +7,13 @@ const Home = () => {
   <>
     <Carousel fade>
       <Carousel.Item>
-          <img src="https://www.internetcommercesummit.com/storage/2023/09/ecommerce1.jpg" alt="placeholder" text="First slide" />
+          <img src="https://www.vocso.com/blog/wp-content/uploads/2022/02/eCommerce-Website-Features-1920-x-1080.jpg" alt="placeholder" text="Third slide" />
       </Carousel.Item>
       <Carousel.Item>
           <img src="https://pix4free.org/assets/library/2021-06-16/originals/commercial.jpg" alt="placeholder" text="Second slide" />
       </Carousel.Item>
       <Carousel.Item>
-          <img src="https://www.vocso.com/blog/wp-content/uploads/2022/02/eCommerce-Website-Features-1920-x-1080.jpg" alt="placeholder" text="Third slide" />
+          <img src="https://www.internetcommercesummit.com/storage/2023/09/ecommerce1.jpg" alt="placeholder" text="First slide" />
       </Carousel.Item>
     </Carousel>
   </>
